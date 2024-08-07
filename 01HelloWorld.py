e34b7c56318292531f94973e6bc8df85ed809f73
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-06 21:14:25 -0300

    first commit

---
+++ 01HelloWorld.py
@@ -0,0 +1,13 @@
+'''
+ 1) Write a program that displays the message "Hello, World!" on the screen.
+'''
+
+def main():
+    def hello_world():
+        print("Hello World !")
+
+    hello_world()
+
+
+main()
+input("Press enter to exit ... ")
